21d4a62cdc82415bc7b083ba3d48a399e46fe055
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2025-11-26 22:41:45 +0100

    Crear turnos

---
+++ database/seeders/ShiftSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Shift;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ShiftSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Shift::updateOrCreate(
+          ['time' => '09:00']
+        );
+
+        Shift::updateOrCreate(
+          ['time' => '15:00']
+        );
+
+        Shift::updateOrCreate(
+          ['time' => '21:00']
+        );
+    }
+}
