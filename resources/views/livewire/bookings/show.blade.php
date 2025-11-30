cd9b10704105d1129a5621f805eaefb62f3d31a5
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2025-11-30 15:10:32 +0100

    Vista de reserva

--- resources/views/livewire/bookings/show.blade.php
+++ resources/views/livewire/bookings/show.blade.php
@@ -15,16 +15,27 @@
               <small class="text-body float-end">{{ __('Shift') }}: {{ $booking->shift->time }}</small>
           </div>
           <div class="card-body">
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>{{ __('Status') }}</strong>: <span class="badge text-bg-{{ $booking->status->class }}">{{ $booking->status->name }}</span></li>
-              <li class="list-group-item"><strong>{{ __('Date') }}</strong>: {{ $booking->date->format('d/m/Y') }}</li>
-              <li class="list-group-item"><strong>{{ __('Shift') }}</strong>: {{ $booking->shift->time }}</li>
-              <li class="list-group-item"><strong>{{ __('Name') }}</strong>: {{ $booking->name }}</li>
-              <li class="list-group-item"><strong>{{ __('Surname') }}</strong>: {{ $booking->surname }}</li>
-              <li class="list-group-item"><strong>{{ __('Email') }}</strong>: {{ $booking->email }}</li>
-              <li class="list-group-item"><strong>{{ __('Phone') }}</strong>: {{ $booking->phone }}</li>
-              <li class="list-group-item"><strong>{{ __('Member number') }}</strong>: {{ $booking->member_number }}</li>
-            </ul>
+            <div class="row row-cols-1 row-cols-lg-2">
+              <div class="col">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><strong>{{ __('Status') }}</strong>: <span class="badge text-bg-{{ $booking->status->class }}">{{ $booking->status->name }}</span></li>
+                  <li class="list-group-item"><strong>{{ __('Date') }}</strong>: {{ $booking->date->format('d/m/Y') }}</li>
+                  <li class="list-group-item"><strong>{{ __('Shift') }}</strong>: {{ $booking->shift->time }}</li>
+                  <li class="list-group-item"><strong>{{ __('Name') }}</strong>: {{ $booking->name }}</li>
+                  <li class="list-group-item"><strong>{{ __('Surname') }}</strong>: {{ $booking->surname }}</li>
+                  <li class="list-group-item"><strong>{{ __('Member number') }}</strong>: {{ $booking->member_number }}</li>
+                </ul>
+              </div>
+              <div class="col">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><strong>{{ __('Email') }}</strong>: {{ $booking->email }}</li>
+                  <li class="list-group-item"><strong>{{ __('Phone') }}</strong>: {{ $booking->phone }}</li>
+                  <li class="list-group-item"><strong>{{ __('Number of guests') }}</strong>: {{ $booking->number_of_guests }}</li>
+                  <li class="list-group-item"><strong>{{ __('Observations') }}</strong>: {{ $booking->observations }}</li>
+                  <li class="list-group-item"><strong>{{ __('Created date') }}</strong>: {{ $booking->created_at->format('d/m/Y H:i') }}</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
