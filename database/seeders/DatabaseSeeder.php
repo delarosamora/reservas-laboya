404205d173f755adc34973ceeba9b770df05fa11
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2025-12-15 18:06:12 +0100

    Crear usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,8 +21,14 @@ class DatabaseSeeder extends Seeder
           ShiftSeeder::class,
       ]);
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Álvaro de la Rosa Mora',
+            'email' => 'alvarodelarosamora@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+                User::factory()->create([
+            'name' => 'Jose Ramón González García',
+            'email' => 'jr-gg@hotmail.com',
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
