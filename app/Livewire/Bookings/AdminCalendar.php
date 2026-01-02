e152b99d8f5c2df0d13a5463f31967d2ef8dee38
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2026-01-02 14:56:01 +0100

    Refactorizar calendarios

--- app/Livewire/Bookings/AdminCalendar.php
+++ app/Livewire/Bookings/AdminCalendar.php
@@ -2,26 +2,8 @@
 
 namespace App\Livewire\Bookings;
 
-use App\Models\Booking;
-use App\Models\BookingStatus;
-use Asantibanez\LivewireCalendar\LivewireCalendar;
-use Illuminate\Support\Collection;
-
-class AdminCalendar extends LivewireCalendar
+class AdminCalendar extends Calendar
 {
-
-  public function events() : Collection
-  {
-    return Booking::whereNot('status_id', BookingStatus::CANCELLED)->orderBy('date', 'desc')->orderBy('shift_id')->get()
-    ->map(function ($booking) {
-      return [
-        'id' => $booking->id,
-        'title' => $booking->name . ' ' . $booking->surname,
-        'description' => $booking->shift->time,
-        'date' => $booking->date,
-      ];
-    });
-  }
 
   public function onEventClick($eventId)
   {
--- app/Livewire/Bookings/PublicCalendar.php
+++ app/Livewire/Bookings/PublicCalendar.php
@@ -4,26 +4,10 @@ namespace App\Livewire\Bookings;
 
 use App\Livewire\Public\CreateBooking;
 use App\Models\Booking;
-use App\Models\BookingStatus;
-use Asantibanez\LivewireCalendar\LivewireCalendar;
 use Carbon\Carbon;
-use Illuminate\Support\Collection;
 
-class PublicCalendar extends LivewireCalendar
+class PublicCalendar extends Calendar
 {
-
-  public function events() : Collection
-  {
-    return Booking::whereNot('status_id', BookingStatus::CANCELLED)->orderBy('date', 'desc')->orderBy('shift_id')->get()
-    ->map(function ($booking) {
-      return [
-        'id' => $booking->id,
-        'title' => $booking->name . ' ' . $booking->surname,
-        'description' => $booking->shift->time,
-        'date' => $booking->date,
-      ];
-    });
-  }
 
   public function onEventClick($eventId)
   {
