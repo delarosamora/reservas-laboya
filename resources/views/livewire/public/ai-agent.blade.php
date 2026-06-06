0e36a5de00d2b549f4340ae7c61372c1c787e410
Alvaro de la Rosa <alvarodelarosamora@gmail.com>
2026-06-06 20:39:55 +0200

    WIP: Mejorando UI

--- resources/views/livewire/public/ai-agent.blade.php
+++ resources/views/livewire/public/ai-agent.blade.php
@@ -1,17 +1,36 @@
 <div>
-
-  <ul>
+    <div class="row py-3">
+    <div class="col text-start">
+      @include('partials.breadcrumbs')
+    </div>
+    <div class="col text-end">
+      <a href="{{ route('home') }}" class="btn btn-secondary" wire:navigate><i class="menu-icon tf-icons bx bx-arrow-back"></i> {{ __('Back') }}</a>
+    </div>
+  </div>
+  <div>
+  <div class="row row-cols-1 border border-light rounded p-2 overflow-scroll" style="height: 60vh">
   @foreach ($messages as $message)
 
-  <li>{{ $message['role'] }}: {!! Str::markdown($message['content']) !!}</li>
+    @if ($message['role'] == 'user')
+    <div class="col bg-light p-2 rounded ms-auto w-auto my-1" style="height: fit-content">{{ $message['content'] }}</div>
+    @else
+    <div class="col my-1">{!! Str::markdown($message['content']) !!}</div>
+    @endif
 
   @endforeach
-  </ul>
+  </div>
   <form wire:submit="sendMessage">
 
-    <input type="text" wire:model="input">
-
-    <button type="submit">Save</button>
+    <div class="row mt-1">
+      <div class="col-10">
+        <x-forms.text-input wire:model="input" placeholder="{{ __('Ask a question ...') }}" />
+      </div>
+      <div class="col-2">
+        <button class="btn btn-primary" type="submit"><i class="icon-base bx bx-send"></i></button>
+      </div>
+    </div>
 </form>
+  </div>
+
 </div>
 
